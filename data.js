91cf878717e0148e43f7768d90b1bf07a9402573
GaNon <hoangan01as@gmail.com>
2025-07-07 02:01:45 +0700

    update 1

---
+++ data.js
@@ -0,0 +1,37 @@
+const animeData = [
+  {
+    title: "Shingeki no Kyojin",
+    rank: 1,
+    image: "https://gogocdn.net/images/anime/Shingeki-no-Kyojin.jpg",
+    episodes: 25,
+    rating: 8.53,
+  },
+  {
+    title: "Steins;Gate",
+    rank: 2,
+    image: "https://gogocdn.net/images/anime/S/steins-gate.jpg",
+    episodes: 24,
+    rating: 9.11,
+  },
+  {
+    title: "Fullmetal Alchemist: Brotherhood",
+    rank: 3,
+    image: "https://gogocdn.net/images/anime/F/Fullmetal-Alchemist-Brotherhood.jpg",
+    episodes: 64,
+    rating: 9.25,
+  },
+  {
+    title: "Hunter x Hunter (2011)",
+    rank: 4,
+    image: "https://gogocdn.net/images/anime/H/Hunter-x-Hunter-2011.jpg",
+    episodes: 148,
+    rating: 9.13,
+  },
+  {
+    title: "Death Note",
+    rank: 5,
+    image: "https://gogocdn.net/images/anime/D/Death-Note.jpg",
+    episodes: 37,
+    rating: 8.63,
+  },
+];
