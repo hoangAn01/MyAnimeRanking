d908f418809c4f9d8e7198156488bf892af3cfbe
GaNon <hoangan01as@gmail.com>
2025-08-08 05:00:56 +0700

    Render fix

--- public/script.js
+++ public/script.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const apiUrl = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
   ? 'http://localhost:3000/api/animes'
-  : 'https://myanimeranking.onrender.com/';
+  : 'https://myanimeranking.onrender.com/api/animes';
   fetch(apiUrl)
     .then((response) => {
       if (!response.ok) {
